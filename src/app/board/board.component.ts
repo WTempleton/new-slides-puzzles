a692a7d9a66165a986b44e4b0d93e7523210b486
WilliamTempleton <william.templeton@centrale-marseille.fr>
2020-03-20 11:16:31 +0100

    Coded HTML, CSS and TS files (installed lodash).

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,15 +1,88 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import shuffle from "lodash/shuffle";
 
 @Component({
-  selector: 'app-board',
-  templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  selector: "app-board",
+  templateUrl: "./board.component.html",
+  styleUrls: ["./board.component.css"]
 })
 export class BoardComponent implements OnInit {
+  @Input() numberOfTiles;
+  @Input() size;
+  @Output() boolToEmit = new EventEmitter<boolean>();
 
-  constructor() { }
-
-  ngOnInit(): void {
+  makeMenuVisible(bool) {
+    this.boolToEmit.emit(bool);
   }
 
+  tilesSet: number[] = [...Array(this.numberOfTiles).keys()];
+
+  tiles: number[] = shuffle(this.tilesSet);
+
+  newGame() {
+    this.tiles = shuffle(this.tilesSet);
+  }
+
+  isZero(value) {
+    if (value === 0) {
+      return false;
+    }
+    return true;
+  }
+
+  canSwap(tile) {
+    const tileIndex = this.tiles.indexOf(tile);
+    const zeroIndex = this.tiles.indexOf(0);
+    if (
+      tileIndex === zeroIndex + Math.sqrt(this.tiles.length) ||
+      tileIndex === zeroIndex - Math.sqrt(this.tiles.length)
+    ) {
+      return true;
+    }
+    if (
+      tileIndex === zeroIndex + 1 &&
+      tileIndex % Math.sqrt(this.tiles.length) !== 0
+    ) {
+      return true;
+    }
+    if (
+      tileIndex === zeroIndex - 1 &&
+      tileIndex % Math.sqrt(this.tiles.length) !==
+        Math.sqrt(this.tiles.length) - 1
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  swap(tile) {
+    if (this.canSwap(tile) === true) {
+      const tileIndex = this.tiles.indexOf(tile);
+      const zeroIndex = this.tiles.indexOf(0);
+      this.tiles[tileIndex] = 0;
+      this.tiles[zeroIndex] = tile;
+    }
+  }
+
+  arraysEqual(a, b) {
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  }
+
+  gameOn() {
+    if (this.arraysEqual(this.tiles, this.tilesSet)) {
+      return false;
+    }
+    return true;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.tilesSet = [...Array(this.numberOfTiles).keys()];
+
+    this.tiles = shuffle(this.tilesSet);
+  }
 }
--- src/app/main-menu/main-menu.component.ts
+++ src/app/main-menu/main-menu.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-main-menu',
-  templateUrl: './main-menu.component.html',
-  styleUrls: ['./main-menu.component.css']
+  selector: "app-main-menu",
+  templateUrl: "./main-menu.component.html",
+  styleUrls: ["./main-menu.component.css"]
 })
 export class MainMenuComponent implements OnInit {
+  mainMenuVisible = true;
+  size: string = "";
 
-  constructor() { }
-
-  ngOnInit(): void {
+  getBool(bool) {
+    this.mainMenuVisible = bool;
   }
 
+  makeDisappear() {
+    this.mainMenuVisible = false;
+  }
+
+  numberOfTiles: number = 9;
+
+  eightMode() {
+    this.numberOfTiles = 9;
+    this.size = "grid-container-eight";
+  }
+
+  fifteenMode() {
+    this.numberOfTiles = 16;
+    this.size = "grid-container-fifteen";
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
